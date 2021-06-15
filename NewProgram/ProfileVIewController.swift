020f28ef3d86c16908bfc96d149e33d9db69e9ac
HarryZ10 <hzhu20@georgefox.edu>
2021-06-15 01:55:50 -0700

    Finished MVP + will add more

---
+++ NewProgram/ProfileVIewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileVIewController.swift
+//  NewProgram
+//
+//  Created by CT Oak Student on 6/15/21.
+//  Copyright © 2021 CT Oak Student. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController
+{
+    @IBOutlet var button: UIButton!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        
+    }
+}
