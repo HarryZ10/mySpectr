42e889a04f7dbebd371a239c13eec5530c40336f
HarryZ10 <hzhu20@georgefox.edu>
2021-06-15 01:55:50 -0700

    Finished MVP + will add more

--- NewProgram/MapViewController.swift
+++ NewProgram/MapViewController.swift
@@ -6,12 +6,79 @@
 //  Copyright © 2021 CT Oak Student. All rights reserved.
 //
 
+import MapKit
 import UIKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-        
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var button: UIButton!
+    
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
+        
+    }
+    
+    @IBAction func didTapButton()
+    {
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .camera
+//        picker.delegate = self
+        present(picker, animated: true)
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest // battery intensive
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+        
+    }
+    
+    func render(_ location: CLLocation)
+    {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+        
     }
 }
+
+//extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        picker.dismiss(animated: true, completion: nil)
+//    }
+//    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        
+//        picker.dismiss(animated: true, completion: nil)
+////        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+////            UIImage else {
+////            return
+////        }
+//    }
+//}
